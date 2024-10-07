e85395e41e492bf5a68981bbd3186f413c098628
Mohamed Diaa <mdiea0581@gmail.com>
2024-10-07 20:46:49 +0300

    IR v1.2

--- Src/main.c
+++ Src/main.c
@@ -22,13 +22,33 @@
 
 
 /***************************** Function protype Section *****************************/
-volatile u8 u8StartFlag       = 0;
+volatile u8  u8StartFlag       = 0;
 volatile u32 u32FrameData[50] = {0};
-volatile u8 u8EdgeCounter     = 0;
-
+volatile u8  u8EdgeCounter     = 0;
+volatile u8  u8Data = 0;
 void voidTakeAction(void)
 {
-    asm("NOP");
+
+	 u8Data = 0;
+
+if ((u32FrameData[0] >= 10000) &&( u32FrameData[0] <= 14000)) {
+
+for (u8 i = 0; i < 8; i++) {
+    if ((u32FrameData[17 + i] >= 2000) &&( u32FrameData[17 + i] <= 2300)) {
+        SET_BIT(u8Data, i);
+    } else {
+        CLR_BIT(u8Data, i);
+    }
+}
+}
+else
+{
+	/* Invalid frame*/
+}
+u8StartFlag       = 0;
+u32FrameData[0] = 0;
+u8EdgeCounter     = 0;
+u8Data = 0;
 }
 
 void voidGetFrame(void)
