8517842917a41ff7d96f3b52aa64140148107d75
Mohamed Diaa <mdiea0581@gmail.com>
2024-10-12 17:03:09 +0300

    s

--- Src/main.c
+++ Src/main.c
@@ -18,87 +18,12 @@
 #include "NVIC_interface.h"
 #include "SysTic_interface.h"
 #include "EXTI_interface.h"
-
-
-
 /***************************** Function protype Section *****************************/
-volatile u8  u8StartFlag       = 0;
-volatile u32 u32FrameData[50] = {0};
-volatile u8  u8EdgeCounter     = 0;
-volatile u8  u8Data = 0;
-void voidTakeAction(void)
-{
-
-	 u8Data = 0;
-
-if ((u32FrameData[0] >= 10000) &&( u32FrameData[0] <= 14000)) {
-
-for (u8 i = 0; i < 8; i++) {
-    if ((u32FrameData[17 + i] >= 2000) &&( u32FrameData[17 + i] <= 2300)) {
-        SET_BIT(u8Data, i);
-    } else {
-        CLR_BIT(u8Data, i);
-    }
-}
-}
-else
-{
-	/* Invalid frame*/
-}
-u8StartFlag       = 0;
-u32FrameData[0] = 0;
-u8EdgeCounter     = 0;
-u8Data = 0;
-}
-
-void voidGetFrame(void)
-{
-    if (u8StartFlag == 0)
-    {
-        /* Start Timer */
-        MSysTic_voidinterruptSingle(1000000, voidTakeAction);
-        u8StartFlag = 1;
-    }
-    else
-    {
-        u32FrameData[u8EdgeCounter] = MSysTic_u32GetElapsedTime();
-        MSysTic_voidinterruptSingle(1000000, voidTakeAction);
-        u8EdgeCounter++;
-    }
-}
-
-
-
-
 /***************************** main code Section *****************************/
-
 int main()
 {
-	/* initialize the RCC to get 1MHz */
-	MRCC_voidInitSysClock();
-	MRCC_voidEnablePeripheral(RCC_APB2, 2); /*Enable GPIOA peripheral */
-
-	/* initialize GPIOA peripheral */
-	MGPIO_voidSetPinDirection(MGPIO_GPIOA, MGPIO_PIN0, MGPIO_Input_Floating); /* GPIOA0 floating input*/
-	MGPIO_voidSetPinDirection(MGPIO_GPIOA, MGPIO_PIN1, MGPIO_Output_PP_2MHZ); /* GPIOA1 PP output*/
-	MGPIO_voidSetPinDirection(MGPIO_GPIOA, MGPIO_PIN2, MGPIO_Output_PP_2MHZ); /* GPIOA2 PP output*/
-	MGPIO_voidSetPinDirection(MGPIO_GPIOA, MGPIO_PIN3, MGPIO_Output_PP_2MHZ); /* GPIOA3 PP output*/
-
-	/*EXTI0 initialization*/
-	MEXTI_voidSetCallBack(voidGetFrame);
-	MEXTI_voidInit();		/* EXTI0 and in falling edge mood*/
-
-	/* Enable interrupts in EXTI0*/
-	MNVIC_voidEnableInterrupt(6);
-
-	/* Enable Systick peripheral */
-	MSysTic_voidInit();		/*Enable Systick in AHP/8=1 MHz*/
-
-
 	while(1)
     {
-
-
     }
     return 0;
 }
